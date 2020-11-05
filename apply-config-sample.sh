cda74a15b8c17221a426389964b9d78f407debdd
Manish Katyan <manishkatyan@gmail.com>
2020-11-06 05:21:15 +0530

    See other viewers webcams

--- apply-config-sample.sh
+++ apply-config-sample.sh
@@ -25,6 +25,9 @@ sed -i 's/defaultWelcomeMessageFooter=.*/defaultWelcomeMessageFooter=Use a heads
 echo "Let Moderators unmute users"
 sed -i 's/allowModsToUnmuteUsers=.*/allowModsToUnmuteUsers=true/g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 
+echo "Enable - See other viewers webcams"
+sed -i 's/webcamsOnlyForModerator=.*/webcamsOnlyForModerator=false/g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+
 echo "Mute the class on start"
 sed -i 's/muteOnStart=.*/muteOnStart=true/g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 
